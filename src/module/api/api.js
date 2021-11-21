bd0629868090d307fd96361358a4d59ab6d0eaec
TolgaAcar <30402367+TolgaAcar@users.noreply.github.com>
2021-11-22 00:24:27 +0300

    Update API Url
    https://restcountries.com/ changed its endpoints.
    
    https://restcountries.eu/rest/v2 is old version
    https://restcountries.com/v2 is new version

--- src/module/api/api.js
+++ src/module/api/api.js
@@ -1,6 +1,6 @@
 export default {
 	async fetchData(query) {
-		const url = `https://restcountries.eu/rest/v2/${query}`;
+		const url = `https://restcountries.com/v2/${query}`;
 		const reponse = await fetch(url);
 		const data = await reponse.json();
 		return data;
